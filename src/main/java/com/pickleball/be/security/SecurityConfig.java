e50df8f0e67719e76b4488ef51cf4cb11bc350f8
phuclh <phuclh121003@gmail.com>
2025-06-10 22:43:29 +0700

    fix security config

--- src/main/java/com/pickleball/be/security/SecurityConfig.java
+++ src/main/java/com/pickleball/be/security/SecurityConfig.java
@@ -45,7 +45,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+            "http://localhost:3000",
+            "https://picklenetweb.vercel.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
