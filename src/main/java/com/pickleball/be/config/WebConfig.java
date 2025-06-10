3d8180af84e4c1c41cb119a99a914ce0eb2aab5d
phuclh <phuclh121003@gmail.com>
2025-06-11 00:20:57 +0700

    fix web config

--- src/main/java/com/pickleball/be/config/WebConfig.java
+++ src/main/java/com/pickleball/be/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("https://picklenetweb.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
